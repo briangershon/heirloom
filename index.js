868b4fd4b1d510e13ab83421b7ced85cf98feb31
bcoe <bencoe@gmail.com>
2014-08-11 17:08:36 -0700

    allow AWS credentials to be passed in as args.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 
 var toS3 = require('./tos3'),
     fs = require('fs');
-    
+
 var argv = require('optimist')
     .usage('Backup your family photos (or any file) efficiently to Amazon S3')
     .options('i', {
@@ -21,17 +21,27 @@ var argv = require('optimist')
         alias : 'prepend'
     })
     .describe('p', 'Prepend absolute path to S3 output path. Use with --strip to remove unneeded initial path segments.')
+    .options('d', {
+        alias : 'aws-access-key-id',
+        default: process.env.AWS_ACCESS_KEY_ID
+    })
+    .describe('d', 'AWS Access Key ID')
+    .options('e', {
+        alias : 'aws-secret-access-key',
+        default: process.env.AWS_SECRET_ACCESS_KEY
+    })
+    .describe('e', 'AWS Secret Access Key')
     .demand(['i', 'b'])
     .argv
 ;
 
 var filePathToBackup = argv.i,
-    awsAccessKey = process.env.AWS_ACCESS_KEY_ID,
-    awsSecretKey = process.env.AWS_SECRET_ACCESS_KEY,
+    awsAccessKey = argv.d,
+    awsSecretKey = argv.e,
     awsBucket = argv.b,
     numberOfPathPartsToStrip = argv.s,
     prependText = argv.p;
-    
+
 if (!awsAccessKey || !awsSecretKey) {
     console.log('Missing AWS Credentails.  You need two environment variables defined: AWS_ACCESS_KEY_ID and AWS_SECRET_ACCESS_KEY');
     process.exit(1);
